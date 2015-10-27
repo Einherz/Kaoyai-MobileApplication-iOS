a316a76b7ab99107f259fae452a93e968ddf1437
Amorn <benamorn@gmail.com>
2015-10-27 11:21:45 +0700

    Initial Commit

---
+++ Kaoyai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaoyai
+//
+//  Created by Amorn Apichattanakul on 10/27/15.
+//  Copyright © 2015 Amorn Apichattanakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
