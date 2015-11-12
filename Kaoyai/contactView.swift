3ea0e6f0234c94302a2d913c9458294ff5fc3654
Amorn <benamorn@gmail.com>
2015-11-12 22:37:15 +0700

    Updated

---
+++ Kaoyai/contactView.swift
@@ -0,0 +1,9 @@
+//
+//  contactView.swift
+//  Kaoyai
+//
+//  Created by Amorn Apichattanakul on 11/12/15.
+//  Copyright © 2015 Amorn Apichattanakul. All rights reserved.
+//
+
+import Foundation
---
+++ Kaoyai/howtoView.swift
@@ -0,0 +1,9 @@
+//
+//  howtoView.swift
+//  Kaoyai
+//
+//  Created by Amorn Apichattanakul on 11/12/15.
+//  Copyright © 2015 Amorn Apichattanakul. All rights reserved.
+//
+
+import Foundation
